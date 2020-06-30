a5fdd51cef9773ff3396cea892f6e9e3a0e2a805
Jean-Olivier LUCRY <jlucry@yahoo.com>
2020-06-30 10:59:33 +0200

    move init logging to ConfigureServices

--- Dev/src/ng-admin/Startup.cs
+++ Dev/src/ng-admin/Startup.cs
@@ -16,13 +16,15 @@ namespace ng_admin
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConsole();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
